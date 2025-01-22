b7a6edb11bd7c899090d480d23a3e88e93a012d7
Rustum Jordan <rustumjordan@gmail.com>
2025-01-23 00:38:00 +0800

    sidemenu responsive

--- client/src/components/sidemenu.tsx
+++ client/src/components/sidemenu.tsx
@@ -8,35 +8,86 @@ import { Link } from "react-router-dom";
 
 const Sidemenu = (): JSX.Element => {
   return (
-    <div className="w-100 h-100 flex-shrink-0 p-3 text-white bg-dark min-vw-50" style={{'overflow': 'auto'}}>
-      <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-        <BsAmd />
-        <span className="fs-4 ms-4 d-block">Dashboard</span>
+    // <div className="w-100 h-100 flex-shrink-0 p-3 text-white bg-dark min-vw-50" style={{'overflow': 'auto'}}>
+    //   <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+    //     <BsAmd />
+    //     <span className="fs-4 ms-4 d-block">Dashboard</span>
+    //   </a>
+    // <hr />
+    // <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+    //   <li className="nav-item">
+    //     <a href="#" className="nav-link active" aria-current="page">
+    //     <FaHome /> &nbsp; Home
+    //     </a>
+    //   </li>
+    //   <li>
+    //     <a href="#" className="nav-link px-0 align-middle">
+    //     <AiOutlineDashboard /> &nbsp; Dashboard
+    //     </a>
+    //   </li>
+    //   <li>
+    //     <Link to="/dashboard/genre" className="nav-link px-0 align-middle"><MdOutlineCategory /> &nbsp; Genre</Link>
+    //   </li>
+    //   <li>
+    //     <Link to="/dashboard/tags" className="nav-link px-0 align-middle"><FaTags /> &nbsp; Tags</Link>
+    //   </li>
+    //   <li>
+    //     <a href="#" className="nav-link px-0 align-middle">
+    //     <MdOutlineLocalMovies /> &nbsp; Movie
+    //     </a>
+    //   </li>
+    // </ul>
+    // </div>
+
+    // <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white h-100 bg-dark" style={{'overflow': 'auto'}}>
+
+    
+<div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100 bg-dark w-100 h-100">
+    <div className="d-none d-sm-inline-block container-fluid">
+      <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none w-100 d-none d-sm-inline-block">
+        <strong className="fs-5 d-block align-middle text-center w-100 mt-3">MOVIE APP</strong>
       </a>
+    </div>
     <hr />
-    <ul className="nav nav-pills flex-column mb-auto">
-      <li className="nav-item">
-        <a href="#" className="nav-link active" aria-current="page">
-        <FaHome /> &nbsp; Home
-        </a>
-      </li>
-      <li>
-        <a href="#" className="nav-link text-white">
-        <AiOutlineDashboard /> &nbsp; Dashboard
-        </a>
-      </li>
-      <li>
-        <Link to="/dashboard/genre" className="nav-link text-white"><MdOutlineCategory /> &nbsp; Genre</Link>
-      </li>
-      <li>
-        <Link to="/dashboard/tags" className="nav-link text-white"><FaTags /> &nbsp; Tags</Link>
-      </li>
-      <li>
-        <a href="#" className="nav-link text-white">
-        <MdOutlineLocalMovies /> &nbsp; Movie
-        </a>
-      </li>
-    </ul>
+      <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+          <li className="nav-item">
+            <a href="#" className="nav-link align-middle px-0 text-white" aria-current="page">
+              <FaHome />  <span className="ms-2 d-none d-sm-inline-block">Home</span>
+            </a>
+          </li>
+          <li>
+            <a href="#" className="nav-link px-0 align-middle text-white">
+              <AiOutlineDashboard /> <span className="ms-2 d-none d-sm-inline-block">Dashboard</span>
+            </a>
+          </li>
+          <li>
+              <Link to="/dashboard/genre" className="nav-link px-0 align-middle text-white"><MdOutlineCategory /> <span className="ms-2 d-none d-sm-inline-block">Genre</span></Link>
+          </li>
+          <li>
+            <Link to="/dashboard/tags" className="nav-link px-0 align-middle text-white"><FaTags /> <span className="ms-2 d-none d-sm-inline-block">Tags</span></Link>
+          </li>
+          <li>
+            <a href="#" className="nav-link px-0 align-middle text-white">
+              <MdOutlineLocalMovies /> <span className="ms-2 d-none d-sm-inline-block">Movie</span>
+            </a>
+          </li>
+      </ul>
+      <hr />
+      <div className="dropdown pb-4">
+          <a href="#" className="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+              <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30" className="rounded-circle" />
+              <span className="d-none d-sm-inline mx-1">loser</span>
+          </a>
+          <ul className="dropdown-menu dropdown-menu-dark text-small shadow">
+              <li><a className="dropdown-item" href="#">New project...</a></li>
+              <li><a className="dropdown-item" href="#">Settings</a></li>
+              <li><a className="dropdown-item" href="#">Profile</a></li>
+              <li>
+                  <hr className="dropdown-divider" />
+              </li>
+              <li><a className="dropdown-item" href="#">Sign out</a></li>
+          </ul>
+        </div>
     </div>
   );
 };
--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -12,12 +12,12 @@ const Dashboard = () => {
   return (<>
     <Container fluid className="p-0">
       <Row className="w-100 d-flex align-items-stretch">
-        <Col xl={3} lg={4} md={4} sm={4} className="col-stretch">
+        <Col xl={3} lg={4} md={4} sm={4} xs={2} className="col-stretch">
           <aside className="sidebar h-100">
             <Sidemenu />
           </aside>
         </Col>
-        <Col xl={9} lg={8} md={8} sm={8} className="col-stretch" style={{'height': '1000px'}}>
+        <Col xl={9} lg={8} md={8} sm={8} xs={10} className="col-stretch" style={{'height': '1000px'}}>
         <main className="main-content">
           <Routes>
             <Route path='/tags' element={<Tags />} />
