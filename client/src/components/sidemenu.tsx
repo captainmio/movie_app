4065d9a0a125046662b8ffa217db22b9493ffa1c
Rustum Jordan <rustumjordan@gmail.com>
2025-01-23 00:46:08 +0800

    convert bootstrap to react-bootstrap side menu

--- client/src/components/sidemenu.tsx
+++ client/src/components/sidemenu.tsx
@@ -1,95 +1,72 @@
-import { BsAmd } from "react-icons/bs";
-import { FaHome } from "react-icons/fa";
-import { AiOutlineDashboard } from "react-icons/ai";
-import { MdOutlineCategory } from "react-icons/md";
-import { FaTags } from "react-icons/fa6";
-import { MdOutlineLocalMovies } from "react-icons/md";
+import React from "react";
+import { Navbar, Nav, Container, Dropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { FaHome, FaTags } from "react-icons/fa";
+import { AiOutlineDashboard } from "react-icons/ai";
+import { MdOutlineCategory, MdOutlineLocalMovies } from "react-icons/md";
 
-const Sidemenu = (): JSX.Element => {
+const Sidebar = () => {
   return (
-    // <div className="w-100 h-100 flex-shrink-0 p-3 text-white bg-dark min-vw-50" style={{'overflow': 'auto'}}>
-    //   <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-    //     <BsAmd />
-    //     <span className="fs-4 ms-4 d-block">Dashboard</span>
-    //   </a>
-    // <hr />
-    // <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
-    //   <li className="nav-item">
-    //     <a href="#" className="nav-link active" aria-current="page">
-    //     <FaHome /> &nbsp; Home
-    //     </a>
-    //   </li>
-    //   <li>
-    //     <a href="#" className="nav-link px-0 align-middle">
-    //     <AiOutlineDashboard /> &nbsp; Dashboard
-    //     </a>
-    //   </li>
-    //   <li>
-    //     <Link to="/dashboard/genre" className="nav-link px-0 align-middle"><MdOutlineCategory /> &nbsp; Genre</Link>
-    //   </li>
-    //   <li>
-    //     <Link to="/dashboard/tags" className="nav-link px-0 align-middle"><FaTags /> &nbsp; Tags</Link>
-    //   </li>
-    //   <li>
-    //     <a href="#" className="nav-link px-0 align-middle">
-    //     <MdOutlineLocalMovies /> &nbsp; Movie
-    //     </a>
-    //   </li>
-    // </ul>
-    // </div>
-
-    // <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white h-100 bg-dark" style={{'overflow': 'auto'}}>
-
-    
-<div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100 bg-dark w-100 h-100">
-    <div className="d-none d-sm-inline-block container-fluid">
-      <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none w-100 d-none d-sm-inline-block">
-        <strong className="fs-5 d-block align-middle text-center w-100 mt-3">MOVIE APP</strong>
-      </a>
-    </div>
-    <hr />
-      <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
-          <li className="nav-item">
-            <a href="#" className="nav-link align-middle px-0 text-white" aria-current="page">
-              <FaHome />  <span className="ms-2 d-none d-sm-inline-block">Home</span>
-            </a>
-          </li>
-          <li>
-            <a href="#" className="nav-link px-0 align-middle text-white">
-              <AiOutlineDashboard /> <span className="ms-2 d-none d-sm-inline-block">Dashboard</span>
-            </a>
-          </li>
-          <li>
-              <Link to="/dashboard/genre" className="nav-link px-0 align-middle text-white"><MdOutlineCategory /> <span className="ms-2 d-none d-sm-inline-block">Genre</span></Link>
-          </li>
-          <li>
-            <Link to="/dashboard/tags" className="nav-link px-0 align-middle text-white"><FaTags /> <span className="ms-2 d-none d-sm-inline-block">Tags</span></Link>
-          </li>
-          <li>
-            <a href="#" className="nav-link px-0 align-middle text-white">
-              <MdOutlineLocalMovies /> <span className="ms-2 d-none d-sm-inline-block">Movie</span>
-            </a>
-          </li>
-      </ul>
+    <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100 bg-dark w-100 h-100">
+      <Container fluid className="d-none d-sm-inline-block">
+        <Navbar.Brand href="/" className="text-white text-decoration-none w-100 d-flex justify-content-center mt-3">
+          <strong className="fs-5 text-center">MOVIE APP</strong>
+        </Navbar.Brand>
+      </Container>
       <hr />
-      <div className="dropdown pb-4">
-          <a href="#" className="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-              <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30" className="rounded-circle" />
-              <span className="d-none d-sm-inline mx-1">loser</span>
-          </a>
-          <ul className="dropdown-menu dropdown-menu-dark text-small shadow">
-              <li><a className="dropdown-item" href="#">New project...</a></li>
-              <li><a className="dropdown-item" href="#">Settings</a></li>
-              <li><a className="dropdown-item" href="#">Profile</a></li>
-              <li>
-                  <hr className="dropdown-divider" />
-              </li>
-              <li><a className="dropdown-item" href="#">Sign out</a></li>
-          </ul>
-        </div>
+      <Nav defaultActiveKey="/" className="flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start">
+        <Nav.Item>
+          <Nav.Link href="#" className="text-white">
+            <FaHome /> <span className="ms-2 d-none d-sm-inline-block">Home</span>
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="#" className="text-white">
+            <AiOutlineDashboard /> <span className="ms-2 d-none d-sm-inline-block">Dashboard</span>
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Link to="/dashboard/genre" className="nav-link text-white">
+            <MdOutlineCategory /> <span className="ms-2 d-none d-sm-inline-block">Genre</span>
+          </Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Link to="/dashboard/tags" className="nav-link text-white">
+            <FaTags /> <span className="ms-2 d-none d-sm-inline-block">Tags</span>
+          </Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="#" className="text-white">
+            <MdOutlineLocalMovies /> <span className="ms-2 d-none d-sm-inline-block">Movie</span>
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <hr />
+      <Dropdown className="pb-4">
+        <Dropdown.Toggle
+          as="span"
+          href="#"
+          className="d-flex align-items-center text-white text-decoration-none"
+          id="dropdownUser1"
+          role='button'
+        >
+          <img
+            src="https://github.com/mdo.png"
+            alt="hugenerd"
+            width="30"
+            height="30"
+            className="rounded-circle"
+          />
+          <span className="d-none d-sm-inline mx-1">loser</span>
+        </Dropdown.Toggle>
+        <Dropdown.Menu variant="dark">
+          <Dropdown.Item href="#">Profile</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item href="#">Sign out</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
     </div>
   );
 };
 
-export default Sidemenu;
+export default Sidebar;
